7bca83288e08b0e2377846b086d795f39a1ad89a
Eric Castro <castro.eric@hotmail.com>
2023-01-05 20:38:56 -0300

    creating an import.sql and adjust id to start with 5

--- src/main/java/com/example/grpc/domain/UserEntity.java
+++ src/main/java/com/example/grpc/domain/UserEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Table(name = "USERS")
 public class UserEntity {
     @Id
-    @SequenceGenerator(name = "users_id_seq", allocationSize = 1)
+    @SequenceGenerator(name = "users_id_seq", allocationSize = 1, initialValue = 5)
     @GeneratedValue(generator = "users_id_seq")
     private Long id;
     private String name;
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, name, email) VALUES (1, 'User1', 'user1@example.com');
+INSERT INTO users (id, name, email) VALUES (2, 'User2', 'user2@example.com');
+INSERT INTO users (id, name, email) VALUES (3, 'User3', 'user3@example.com');
+INSERT INTO users (id, name, email) VALUES (4, 'User4', 'user4@example.com');
