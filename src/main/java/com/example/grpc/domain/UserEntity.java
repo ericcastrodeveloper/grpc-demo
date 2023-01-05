c1c9242a495dcb13995624ee88b9e41284087df8
Eric Castro <castro.eric@hotmail.com>
2023-01-05 20:36:31 -0300

    creating a domain class

---
+++ src/main/java/com/example/grpc/domain/UserEntity.java
@@ -0,0 +1,19 @@
+package com.example.grpc.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "USERS")
+public class UserEntity {
+    @Id
+    @SequenceGenerator(name = "users_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "users_id_seq")
+    private Long id;
+    private String name;
+    private String email;
+}
